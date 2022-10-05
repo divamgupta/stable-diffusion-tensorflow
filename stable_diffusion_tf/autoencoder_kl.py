6e274170eb067c9ea1e68f66e9ad1d5fb9b47a60
Lime-Cakes <91322985+Lime-Cakes@users.noreply.github.com>
2022-10-06 03:05:31 +0800

    Switch to DownsampleConv2D
    This changes downsampling used by Encoder to pad with asymmetric  padding (pad only right and bottom)

--- stable_diffusion_tf/autoencoder_kl.py
+++ stable_diffusion_tf/autoencoder_kl.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import tensorflow_addons as tfa
 
-from .layers import apply_seq, PaddedConv2D
+from .layers import apply_seq, PaddedConv2D, DownsampleConv2D
 
 
 class AttentionBlock(keras.layers.Layer):
@@ -98,15 +98,15 @@ class Encoder(keras.Sequential):
                 PaddedConv2D(128, 3, padding=1 ),
                 ResnetBlock(128,128),
                 ResnetBlock(128, 128),
-                PaddedConv2D(128 , 3 ,  padding=0, stride=2),
+                DownsampleConv2D(128 , 3 , stride=2),
                 
                 ResnetBlock(128,256),
                 ResnetBlock(256, 256),
-                PaddedConv2D(256 , 3 ,  padding=0, stride=2),
+                DownsampleConv2D(256 , 3 , stride=2),
                 
                 ResnetBlock(256,512),
                 ResnetBlock(512, 512),
-                PaddedConv2D(512 , 3 ,  padding=0, stride=2),
+                DownsampleConv2D(512 , 3 , stride=2),
                 
                 ResnetBlock(512,512),
                 ResnetBlock(512, 512),
