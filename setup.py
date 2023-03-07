fdbfd405855c8f3644a6a00d320bc1d014317cd4
The DT <80153297+thedtvn@users.noreply.github.com>
2023-03-07 17:49:35 +0700

    add auto install requirements and check is mac m1

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import find_packages, setup
+import platform, sys
+
+
+with open('requirements.txt') as f: # default requirements
+    install_requires = [i.strip() for i in f.read().splitlines()]
+
+if platform.processor() == "arm" and sys.platform == "darwin": # check is mac chip m1
+    with open('requirements_m1.txt') as f:
+        install_requires = [i.strip() for i in f.read().splitlines()]
 
 setup(
     name="stable_diffusion_tf",
@@ -7,6 +16,7 @@ setup(
     author="Divam Gupta",
     author_email="guptadivam@gmail.com",
     platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+    install_requires=install_requires, # install requirements
     url="https://github.com/divamgupta/stable-diffusion-tensorflow",
     packages=find_packages(),
 )
